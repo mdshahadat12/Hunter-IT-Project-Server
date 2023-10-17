bb98cc651b2fa2e0c532671f203b0f95a7999895
Md Shahadat <mdsadt2@gmail.com>
2023-10-17 23:18:10 +0600

    intial setup

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+// hunterit
+// lYxmTPY7SrapRMuT
+
+
+// middlewere
+app.use(express.json())
+app.use(cors())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://hunterit:lYxmTPY7SrapRMuT@cluster0.dsq3s3c.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productbd = client.db('product');
+    const productCollection = productbd.collection('product')
+
+    app.post('/google/product',(req,res)=>{
+        const product = req.body;
+        const result = productCollection.insertOne(product)
+        res.send(result)
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Running')
+})
+
+app.listen(port,()=>{
+    console.log(`server in running on port ${port}`);
+})
