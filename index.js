13bd06a7ecaceba5b5f15827f458152ac5872d4d
Md Shahadat <mdsadt2@gmail.com>
2023-10-21 19:29:29 +0600

    data load problem fix

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productdb = client.db('product');
     const productCollection = productdb.collection('product')
@@ -97,7 +97,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -109,7 +109,7 @@ run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
-    res.send('Running Changed')
+    res.send('Running t1')
 })
 
 app.listen(port,()=>{
